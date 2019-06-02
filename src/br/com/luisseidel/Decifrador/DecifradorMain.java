e84168a77c33e1ec26a5786e4ea2e217beef77cf
luisseidel <luis.guilherme.seidel@gmail.com>
2019-06-01 21:28:30 -0300

    master

--- src/br/com/luisseidel/Decifrador/DecifradorMain.java
+++ src/br/com/luisseidel/Decifrador/DecifradorMain.java
@@ -1,7 +1,10 @@
 package br.com.luisseidel.Decifrador;
 
+import com.google.gson.Gson;
+
 public class DecifradorMain {
 	public static void main(String[] args) {
+		
 		System.out.println(Decifrador.decifrar(10, "ofobi lsq mywzedsxq nsckcdob rkc mywo pbyw dkusxq dyy wkxi snokc kxn zeddsxq drow sx yxo zvkmo. qybnyx lovv"));
 	}
 }
