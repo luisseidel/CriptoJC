0f79c02cb9914a7c31f55aaa8087e03bb22c6c26
Luis Guilherme Seidel <luis.guilherme.seidel@gmail.com>
2019-06-03 17:15:01 -0300

    asd

--- src/br/com/luisseidel/Decifrador/DecifradorMain.java
+++ src/br/com/luisseidel/Decifrador/DecifradorMain.java
@@ -1,29 +1,39 @@
 package br.com.luisseidel.Decifrador;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class DecifradorMain {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-			
+		
+		//Ler Objeto JSON
 		JSONObject my_obj;
 		JSONParser parser = new JSONParser();
 		ObjetoDecifrar objDec = new ObjetoDecifrar();
 		
 		try {
 			my_obj = (JSONObject) parser.parse(new FileReader("answer.json"));
-			objDec.setNumeroCasas(Long.toString((long) my_obj.get("numero_casas")));
+			objDec.setNumeroCasas((String) my_obj.get("numeroCasas"));
 			objDec.setToken((String) my_obj.get("token"));
-			objDec.setTextoCifrado((String) my_obj.get("cifrado"));
-			objDec.setTextoDecifrado(Decifrador.decifrar(Long.toString((long) my_obj.get("numero_casas")), objDec.getTextoCifrado()));
+			objDec.setTextoCifrado((String) my_obj.get("textoCifrado"));
+			objDec.setTextoDecifrado(Decifrador.decifrar((String) my_obj.get("numeroCasas"), objDec.getTextoCifrado()));
 			objDec.setResumoCriptografico(ResumoCriptograficoSHA1.stringHexa(ResumoCriptograficoSHA1.gerarHash(objDec.getTextoDecifrado(), "SHA-1")));
-		
-			System.out.println("VALROES RECUPERADOS DO JSON");
-			System.out.println(objDec.toString());
 		
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -32,7 +42,46 @@ public class DecifradorMain {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+			
+		
+		//Escrever Objeto JSON
+		FileWriter writeFile = null;
+		JSONObject objJson = new JSONObject();
+		
+		objJson.put("numeroCasas", objDec.getNumeroCasas());
+		objJson.put("token", objDec.getToken());
+		objJson.put("textoCifrado", objDec.getTextoCifrado());
+		objJson.put("textoDecifrado", objDec.getTextoDecifrado());
+		objJson.put("resumoCriptografico", objDec.getResumoCriptografico());
+		
+		System.out.println(objJson.toJSONString());
+		
+		try {
+			writeFile = new FileWriter("answer.json");
+			writeFile.write(objJson.toJSONString());
+			writeFile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		
+		//Enviar Objeto JSON via POST
+		//https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=25f95d6af0cbac6104b8a3dce578b8090e09ba42
+		
+		try {
+			CloseableHttpClient client = HttpClients.createDefault();
+		    HttpPost httpPost = new HttpPost("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=25f95d6af0cbac6104b8a3dce578b8090e09ba42");
+		 
+		    String json = objJson.toJSONString();
+		    StringEntity entity = new StringEntity(json);
+		    httpPost.setEntity(entity);
+		    httpPost.setHeader("Content-type", "multipart/form-data");
+		 
+		    CloseableHttpResponse response = client.execute(httpPost);
+		    System.out.println(response.getStatusLine().getStatusCode());
+		    client.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
