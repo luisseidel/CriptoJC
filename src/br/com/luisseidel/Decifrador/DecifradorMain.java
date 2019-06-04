c300b8bda0fbd790a7f550dbbd19cba833c17cfa
luisseidel <luis.guilherme.seidel@gmail.com>
2019-06-03 23:18:02 -0300

    asd

--- src/br/com/luisseidel/Decifrador/DecifradorMain.java
+++ src/br/com/luisseidel/Decifrador/DecifradorMain.java
@@ -3,12 +3,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -63,21 +63,25 @@ public class DecifradorMain {
 		//Enviar Objeto JSON via POST
 		//https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=25f95d6af0cbac6104b8a3dce578b8090e09ba42
 		
+		URL url;
+		
 		try {
-			CloseableHttpClient client = HttpClients.createDefault();
-		    HttpPost httpPost = new HttpPost("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=25f95d6af0cbac6104b8a3dce578b8090e09ba42");
-		 
-		    String json = objJson.toJSONString();
-		    StringEntity entity = new StringEntity(json);
-		    httpPost.setEntity(entity);
-		    httpPost.setHeader("Content-type", "multipart/form-data");
-		 
-		    CloseableHttpResponse response = client.execute(httpPost);
-		    System.out.println(response);
-		    System.out.println(response.getStatusLine().getStatusCode());
-		    client.close();
+			url =new URL("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=25f95d6af0cbac6104b8a3dce578b8090e09ba42");
+			URLConnection con = url.openConnection();
+			HttpURLConnection http = (HttpURLConnection) con;
+			http.setRequestMethod("POST");
+			http.setDoOutput(true);
+			byte[] out = objJson.toString().getBytes(StandardCharsets.UTF_8);
+			int leng = out.length;
+			
+			http.setFixedLengthStreamingMode(leng);
+			http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+			http.connect();
+			OutputStream os = http.getOutputStream();
+			os.write(out);
 		} catch (Exception e) {
-			e.printStackTrace();
+
 		}
 	}
 }
+
