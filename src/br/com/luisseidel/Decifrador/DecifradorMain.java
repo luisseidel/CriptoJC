d74220416a79077ffe3052d216385bb4cf57a6ea
Luis Guilherme Seidel <luis.guilherme.seidel@gmail.com>
2019-06-04 17:36:32 -0300

    asdsadd

--- src/br/com/luisseidel/Decifrador/DecifradorMain.java
+++ src/br/com/luisseidel/Decifrador/DecifradorMain.java
@@ -1,14 +1,16 @@
 package br.com.luisseidel.Decifrador;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStreamReader;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -28,8 +30,8 @@ public class DecifradorMain {
 			objDec.setToken((String) my_obj.get("token"));
 			objDec.setTextoCifrado((String) my_obj.get("textoCifrado"));
 			objDec.setTextoDecifrado(Decifrador.decifrar((String) my_obj.get("numeroCasas"), objDec.getTextoCifrado()));
-			objDec.setResumoCriptografico(ResumoCriptograficoSHA1.stringHexa(ResumoCriptograficoSHA1.gerarHash(objDec.getTextoDecifrado(), "SHA-1")));
-		
+			objDec.setResumoCriptografico(ResumoCriptograficoSHA1.stringHexa(ResumoCriptograficoSHA1.gerarHash(objDec.getTextoDecifrado(), "SHA-1")).toUpperCase());
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -49,7 +51,7 @@ public class DecifradorMain {
 		objJson.put("textoDecifrado", objDec.getTextoDecifrado());
 		objJson.put("resumoCriptografico", objDec.getResumoCriptografico());
 		
-		System.out.println(objJson.toJSONString());
+		System.out.println(objJson);
 		
 		try {
 			writeFile = new FileWriter("answer.json");
@@ -62,26 +64,33 @@ public class DecifradorMain {
 		
 		//Enviar Objeto JSON via POST
 		//https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=25f95d6af0cbac6104b8a3dce578b8090e09ba42
-		
-		URL url;
-		
-		try {
-			url =new URL("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=25f95d6af0cbac6104b8a3dce578b8090e09ba42");
-			URLConnection con = url.openConnection();
-			HttpURLConnection http = (HttpURLConnection) con;
-			http.setRequestMethod("POST");
-			http.setDoOutput(true);
-			byte[] out = objJson.toString().getBytes(StandardCharsets.UTF_8);
-			int leng = out.length;
-			
-			http.setFixedLengthStreamingMode(leng);
-			http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-			http.connect();
-			OutputStream os = http.getOutputStream();
-			os.write(out);
-		} catch (Exception e) {
+		//URL teste ==> https://httpbin.org/post
+		try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
 
-		}
+            HttpPost request = new HttpPost("https://httpbin.org/post");
+            request.setHeader("User-Agent", "Java client");
+            request.addHeader("Content-Type", "multipart/form-data");
+            request.addHeader("file", "answer");
+            request.setEntity(new StringEntity(objJson.toJSONString()));
+
+            HttpResponse response = client.execute(request);
+
+            BufferedReader bufReader = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent()));
+
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+
+            while ((line = bufReader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.lineSeparator());
+            }
+
+            System.out.println(builder);
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
 	}
 }
 
