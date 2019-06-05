150aef5d0ead98ac6afef2ebe2820a1d36a2f0fa
luisseidel <luis.guilherme.seidel@gmail.com>
2019-06-04 22:18:20 -0300

    asd

--- src/br/com/luisseidel/Decifrador/DecifradorMain.java
+++ src/br/com/luisseidel/Decifrador/DecifradorMain.java
@@ -5,6 +5,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -29,8 +33,11 @@ public class DecifradorMain {
 			objDec.setNumeroCasas((String) my_obj.get("numero_casas"));
 			objDec.setToken((String) my_obj.get("token"));
 			objDec.setTextoCifrado((String) my_obj.get("cifrado"));
-			objDec.setTextoDecifrado(Decifrador.decifrar((String) my_obj.get("numero_casas"), objDec.getTextoCifrado()));
-			objDec.setResumoCriptografico(ResumoCriptograficoSHA1.stringHexa(ResumoCriptograficoSHA1.gerarHash(objDec.getTextoDecifrado(), "SHA-1")).toUpperCase());
+			objDec.setTextoDecifrado(Decifrador.decifrar(
+					(String) my_obj.get("numero_casas"), objDec.getTextoCifrado()));
+			objDec.setResumoCriptografico(
+					ResumoCriptograficoSHA1.stringHexa(
+							ResumoCriptograficoSHA1.gerarHash(objDec.getTextoDecifrado(), "SHA-1")).toUpperCase());
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -69,15 +76,24 @@ public class DecifradorMain {
 			e.printStackTrace();
 		}
 		
+		System.out.println("---- ENVIANDO ARQUIVO JSON ----\n");
+		
+		/*try {
+			System.out.println(post(stringJson));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		*/
 		
 		//Enviar Objeto JSON via POST
 		//https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=25f95d6af0cbac6104b8a3dce578b8090e09ba42
 		//URL teste ==> https://httpbin.org/post
+		
+		/*
 		try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
 
             HttpPost request = new HttpPost("https://httpbin.org/post");
-            request.setHeader("User-Agent", "Java client");
-            request.addHeader("Content-Type", "multipart/form-data");
+            request.addHeader("Content-Type", "application/json");
             request.addHeader("file", "answer");
             request.setEntity(new StringEntity(stringJson));
 
@@ -99,6 +115,35 @@ public class DecifradorMain {
         } catch (Exception e) {
         	e.printStackTrace();
         }
+        */
 	}
+	/*
+	public static String post(String json) throws IOException {
+		 
+        URL url = new URL("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=25f95d6af0cbac6104b8a3dce578b8090e09ba42");
+ 
+        HttpURLConnection connection =
+            (HttpURLConnection) url.openConnection();
+ 
+        connection.setRequestMethod("POST");
+ 
+        connection.setRequestProperty("Content-type", "application/json");
+        connection.setRequestProperty("Accept", "application/json");
+ 
+        connection.setDoOutput(true);
+ 
+ 
+        PrintStream printStream =
+            new PrintStream(connection.getOutputStream());
+        printStream.println(json);
+ 
+        connection.connect();
+ 
+        String jsonDeResposta =
+            new Scanner(connection.getInputStream()).next();
+ 
+        return connection.getResponseMessage();
+    }
+*/
 }
 
